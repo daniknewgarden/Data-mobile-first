9f457ef9449bf7000ebadf432238121ff87fdbec
Danik <daniknewgarden@gmail.com>
2020-03-08 21:10:35 +0200

    Ended menu script

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,11 @@ let menuBtn = document.querySelector('.menu-button'),
     link = document.querySelectorAll('.burger-link');
 
 menuBtn.addEventListener('click', function () {
-    links.classList.toggle('active');
+    links.classList.toggle('show');
 });
 
 for (let i = 0; i < link.length; i++) {
     link[i].addEventListener('click', function () {
-        links.classList.add('hide');
+        links.classList.remove('show');
     });
 }
