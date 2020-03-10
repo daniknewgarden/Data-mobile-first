d63dcd473d207191a80bc4a2e11af5f37823e5cd
Danik <daniknewgarden@gmail.com>
2020-03-10 17:01:52 +0200

    Aded new burger button

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
-let menuBtn = document.querySelector('.menu-button'),
+let body = document.body,
+    menuBtn = document.querySelector('.menu-button'),
+    BtnLine = document.querySelectorAll('.line'),
     links = document.querySelector('.menu-links'),
     link = document.querySelectorAll('.burger-link');
 
 menuBtn.addEventListener('click', function () {
+    for (let i = 0; i < BtnLine.length; i++) {
+        BtnLine[i].classList.toggle('open');
+    }
     links.classList.toggle('show');
+    body.classList.toggle('not-overflow');
 });
 
 for (let i = 0; i < link.length; i++) {
